5e29da93c063e7ecfccf819e4d869bb3f3fa75f0
Laszlo J. Szalai <laszlo.szalai@carolcc.com>
2013-10-23 12:11:08 +0200

    Fisrt steps.

--- src/com/comuv/szalai/bazsakpo/MainActivity.java
+++ src/com/comuv/szalai/bazsakpo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.comuv.szalai.bazsakpo;
 
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -8,10 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private Random random;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		random = new Random();
 		setContentView(R.layout.activity_main);
 	}
 
@@ -23,10 +28,16 @@ public class MainActivity extends Activity {
 	}
 	
 	public void onBtnGo(View view) {
+		String NLine = System.getProperty("line.separator");
+		String[] gepVal = {"K", "P", "O", "X"};
 		EditText editText = (EditText) findViewById(R.id.eBet);
-		String message = editText.getText().toString();
+		String jatekos = editText.getText().toString().substring(0, 1).toUpperCase();
+		int kocka = random.nextInt(3);
+		String gep = gepVal[kocka];
+		String status = "A gép választása: " + gep + "," + NLine + "a játékos választása: " + jatekos + "." 
+				+ NLine + "Ezúttal nem nyert senki. (0:0)";
 		TextView tw = (TextView) findViewById(R.id.twStatus);
-		tw.setText(message.toCharArray(), 0, message.length());
+		tw.setText(status.toCharArray(), 0, status.length());
 	}
 
 }
